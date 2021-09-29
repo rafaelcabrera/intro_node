9113f74a3b9037446409bfa3b6b291dda4647a6d
Rafael Cabrera <33456559+rafaelcabrera@users.noreply.github.com>
2021-09-29 10:47:47 -0300

    packages utiles

---
+++ paquetes/modulo/utiles/bcrypt.js
@@ -0,0 +1,16 @@
+const bcrypt = require('bcrypt');
+// import * as bcrypt from 'bcrypt';
+// const saltRounds = 10;
+
+const password = '1234Segura!';
+
+bcrypt.hash(password,5,(err,hash)=>{
+    console.log(hash);
+    bcrypt.compare(password,hash,(err,res)=>{
+        //console.log(err);
+        console.log(res);
+    });
+})//pass ronda y funcion
+
+//tarea que lo guarde en archivo.
+//en vez de callback se puede usar promesa
